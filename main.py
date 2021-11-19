ec0c6500a22abfe74e15a4505a559d51effc7c1f
rimprog <riminprog@gmail.com>
2021-11-19 13:07:56 +0300

    Send logs to Telegram to admin user

--- main.py
+++ main.py
@@ -9,6 +9,18 @@ import telegram
 from dotenv import load_dotenv
 
 
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
+
+
 def get_dvmn_api_response(timestamp):
     response = requests.get(
         'https://dvmn.org/api/long_polling/',
@@ -23,7 +35,7 @@ def get_dvmn_api_response(timestamp):
     return dvmn_api_response
 
 
-def send_telegram_notification(bot, dvmn_api_response):
+def send_telegram_notification(bot, chat_id, dvmn_api_response):
     notifications = {
         'success': '\nУ вас проверили работу "{}".\nСсылка на работу: {}\nПреподавателю все понравилось, можно приступать к следующему уроку!\n',
         'fail': '\nУ вас проверили работу "{}".\nСсылка на работу: {}\nК сожалению в работе нашлись ошибки. Исправьте их и отправьте работу на проверку снова.\n',
@@ -40,18 +52,22 @@ def send_telegram_notification(bot, dvmn_api_response):
 
         bot.send_message(
             text=notification,
-            chat_id=os.getenv('TELEGRAM_USER_ID')
+            chat_id=chat_id
         )
-
-        print('New notification sent in telegram')
 
 
 def main():
     load_dotenv()
+    
     bot = telegram.Bot(token=os.getenv('TELEGRAM_BOT_TOKEN'))
+    chat_id = os.getenv('TELEGRAM_USER_ID')
     timestamp = ''
 
-    logging.warning('Bot started. Waiting new lesson review...')
+    logger = logging.getLogger('Logger')
+    logger.setLevel(logging.INFO)
+    logger.addHandler(TelegramLogsHandler(bot, chat_id))
+
+    logger.info('Bot started. Waiting new lesson review...')
 
     while True:
         try:
@@ -59,7 +75,7 @@ def main():
         except requests.exceptions.ReadTimeout as read_timeout_error:
             continue
         except requests.exceptions.ConnectionError as connection_error:
-            print(connection_error)
+            logger.error(connection_error)
             sleep(100)
 
         if dvmn_api_response['status'] == 'timeout':
@@ -67,7 +83,7 @@ def main():
         elif dvmn_api_response['status'] == 'found':
             timestamp = dvmn_api_response['last_attempt_timestamp']
 
-            send_telegram_notification(bot, dvmn_api_response)
+            send_telegram_notification(bot, chat_id, dvmn_api_response)
 
 
 if __name__ == '__main__':
